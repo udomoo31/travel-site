ae2a92bc75c4414373a8ad75025ccb25d26aaf66
udomoo31 <udomoo31@gmail.com>
2020-03-30 21:31:28 +0800

    done working with 7. Mobile-first Essentials modules of modern workflow...

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,5 @@
+import '../styles/styles.css'
+
+if (module.hot) {
+    module.hot.accept();   //refresh js and css on the browser memory withouth actual refreshing the webpage.
+}
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+
+const postCSSPlugins = [
+  require("postcss-import"),
+  require("postcss-mixins"),
+  require("postcss-simple-vars"),
+  require("postcss-nested"),
+  require("autoprefixer")
+];
+
+module.exports = {
+  entry: "./app/assets/scripts/App.js", // source of the
+
+  // customize the filename of the output and the location
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "app")
+  },
+
+  devServer: {
+      
+    before: function( app, server ){
+        server._watch('./app/**/*.html'); // watch and reload when HTML files changes
+    },
+    contentBase: path.join(__dirname, "app"),
+    hot: true,
+    port: 3000, // port used
+    host: '0.0.0.0' // allow mobile device with same network to be access on phone
+  },
+
+  mode: "development", // or production, minified if production
+//   watch : true, // devServer takes the job
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          { loader: "postcss-loader", options: { plugins: postCSSPlugins } }
+        ]
+      }
+    ]
+  }
+};
