82aac2fb258b136247e14e79368babb78c945b6b
udomoo31 <udomoo31@gmail.com>
2020-04-03 07:47:15 +0800

    Completed the modal Overly

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,13 +2,34 @@ import '../styles/styles.css'
 import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
 import StickyHeader from "./modules/StickyHeader";
+// import Modal from "./modules/Modal";
 
 let stickyHeader = new StickyHeader();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
-// new RevealOnScroll();
-
 let mobileMenu = new MobileMenu();
+let modal;
+
+document.querySelectorAll(".open-modal").forEach( el => {
+    el.addEventListener("click" , e => {
+        e.preventDefault();
+        if (typeof modal === "undefined") {
+            
+            import(/* webpackChunkName: "modalz" */ "./modules/Modal").then(x => {
+                modal = new x.default();
+                setTimeout(() => {
+                    modal.openTheModal();
+                }, 20);
+            }).catch(() => {
+                console.log("There was a problem")
+            }); 
+
+        } else {
+            modal.openTheModal();
+        }       
+    })
+})
+
 
 if (module.hot) {
     module.hot.accept();   //refresh js and css on the browser memory withouth actual refreshing the webpage.
