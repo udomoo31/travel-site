714c6c1afa340c20233d8f061964b7b07b5cb19d
udomoo31 <udomoo31@gmail.com>
2020-04-03 09:54:49 +0800

    Completed lazy loading of images

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,13 +1,15 @@
 import '../styles/styles.css'
+import 'lazysizes';
+
 import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
 import StickyHeader from "./modules/StickyHeader";
 // import Modal from "./modules/Modal";
 
-let stickyHeader = new StickyHeader();
+new StickyHeader();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
-let mobileMenu = new MobileMenu();
+new MobileMenu();
 let modal;
 
 document.querySelectorAll(".open-modal").forEach( el => {
