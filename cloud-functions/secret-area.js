7ae29244e6822f95a60d2409f21c35ed1de0cad4
udomoo31 <udomoo31@gmail.com>
2020-04-06 19:11:41 +0800

    Updated cloud function to check password

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,8 +1,38 @@
 exports.handler = function ( event , context , callback ) {
 
-    callback( null , {
-        statusCode: 200,
-        body : "Welcome to super duper webzit!",
-    });
+    const secretContent = `
+        <h3>Welcome to Secret area!</h3>
+
+        <p>Tama ka sa password! ,<br/><b>Hahahahaha!</b> </p>
+    `;
+
+    let body;
+
+    if ( event.body ) {
+
+        body = JSON.parse( event.body );
+        
+    } else {
+        body = {};
+    }
+
+
+    if (body.password === "javascript") {
+        
+        callback(null, {
+            statusCode: 200,
+            body: secretContent,
+        });
+        
+    } else {
+        
+        callback(null, {
+            statusCode: 401, // 401 means unauthorized
+            // body: "Welcome to super duper webzit!",
+        });
+
+    }
+
+    
 
 }
