4eeb64111b52247c64849f0a8f4505621df71965
udomoo31 <udomoo31@gmail.com>
2020-04-06 22:18:11 +0800

    Added client area feature

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -4,8 +4,10 @@ import 'lazysizes';
 import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
 import StickyHeader from "./modules/StickyHeader";
+import ClientArea from './modules/ClientArea';
 // import Modal from "./modules/Modal";
 
+new ClientArea();
 new StickyHeader();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
